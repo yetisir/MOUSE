0e6dff7b90253923df37925fd778ddba37a02673
yetisir <yetisir@gmail.com>
2016-12-31 01:29:10 -0500

    updated documentation

--- docs/compileManual.bat
+++ docs/compileManual.bat
@@ -2,8 +2,8 @@ REM @ECHO OFF
 
 CALL make html
 CALL make latex
-cd build\latex
+cd _build\latex
 pdflatex MOUSE.tex
 pdflatex MOUSE.tex
 cd ..\..
-COPY "build\latex\MOUSE.pdf" "..\Manual.pdf"
+COPY "_build\latex\MOUSE.pdf" "..\Manual.pdf"
